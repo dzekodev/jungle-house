cfae479210d41f5308284dfedd430832a9e0b6cf
dzekodev <weirdo.high@gmail.com>
2021-07-28 15:04:19 +0100

    last

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import '../styles/App.css';
 import Banner from './Banner';
 import Cart from './Cart';
+import ShoppingList from './ShoppingList'
 function App() {
   return (
     <React.Fragment>
       <Banner/>
       <Cart/>
+      <ShoppingList/>
     </React.Fragment>
   );
 }
---
+++ src/components/ShoppingList.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {plantList} from '../datas/plantList'
+function ShoppingList(){
+    const category = plantList.reduce(
+        (acc,plant)=> acc.includes(plant.category)?acc:acc.concat(plant.category),[]
+    );
+    return(
+        <React.Fragment>
+            <ul>
+                {category.map(cat=>(
+                    <li>{cat}</li>
+                ))}
+            </ul>
+            <ul>
+                {plantList.map(plant=>(
+                    <li key={plant.id}>{plant.name}</li>
+                ))}
+            </ul>
+        </React.Fragment>
+    );
+}
+export default ShoppingList
